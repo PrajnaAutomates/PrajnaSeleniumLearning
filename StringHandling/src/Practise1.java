db7a00a7e739b9fc8335b6f758b124bca83acfc9
Shobha <Shobha@Shobha-PC>
2021-06-19 12:29:21 +0530

    My First Commit

---
+++ StringHandling/src/Practise1.java
@@ -0,0 +1,13 @@
+
+public class Practise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "Pyament $100 paid";
+		System.out.println(str.charAt(8));
+		System.out.println(str.indexOf("$"));
+		System.out.println(str.substring(8,11));
+	}
+
+}
---
+++ StringHandling/src/SUmOfNumbers.java
@@ -0,0 +1,23 @@
+
+public class SUmOfNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {2,5,8,7,4};
+		int n =8;
+		
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+			if (a[i] == n)
+			{
+				System.out.println("The indexof number 8 is :"+i);
+				break;
+			}
+			
+		}
+		
+	
+
+}
+}
